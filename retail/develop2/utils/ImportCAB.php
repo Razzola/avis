e97fbc0fa070442a44ea615ba741654830a4fbb1
Roberto Azzola <r.azzola@reply.it>
2019-07-24 12:39:30 +0200

    FIx date import for discrepancy check

--- retail/develop2/utils/ImportCAB.php
+++ retail/develop2/utils/ImportCAB.php
@@ -16,14 +16,19 @@
                     $resultIdListCAB = $mysqli->query("SELECT COUNT(*) FROM `cab` WHERE rfc ='".$columnCAB[0]."'");
                     $ticketsIdCountCAB = $resultIdListCAB->fetch_row();
                     $ticketsIdTotalCAB = $ticketsIdCountCAB[0];
+                    if ($columnCAB[17]=='' || $columnCAB[17]=='TBD'){
+                        $columnCAB[17] == null;  //stop inserting 1970-01-01
+                    }else{
+                        $columnCAB[17]= date("Y-m-d",strtotime(str_replace('/', '-', $columnCAB[17])));
+                    }
                     //echo "SELECT COUNT(*) FROM `tickets` WHERE id ='".$columnCAB[0]."' AND environment = '".$environment."'";
-                    //echo $ticketsIdCountCAB[0];
+                    //echo $columnCAB[17];
                     if($ticketsIdCountCAB[0]==0){
                         $sqlInsert = "INSERT into cab (rfc,environment,source_id,prod_date) values (
                         " . $columnCAB[0] . ",
                         '" . $columnCAB[1] . "',
                         '" . mysqli_real_escape_string($mysqli, $columnCAB[2]) . "',
-                        '" . date("Y-d-m",strtotime($columnCAB[10])) . "')";
+                        '" . $columnCAB[17] . "')";
                         //echo $columnCAB[0]."<br/>";
                         //echo $sqlInsert."<br/>";
                         $resultCAB = mysqli_query($mysqli, $sqlInsert);
@@ -40,7 +45,7 @@
                         $sqlUpdate = "UPDATE cab
                         SET source_id = '" . mysqli_real_escape_string($mysqli, $columnCAB[2]) . "',
                         environment = '" . $columnCAB[1] . "',
-                        prod_date ='" . date("Y-d-m",strtotime($columnCAB[10]))."'
+                        prod_date ='" . $columnCAB[17]."'
                         WHERE rfc ='".$columnCAB[0]."'";
                         //echo $columnCAB[2];
                         //echo $sqlUpdate."<br/>";
